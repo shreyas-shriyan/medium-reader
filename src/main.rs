04127c87e4f364d76f9ea8064befae19642e56de
shreyas-shriyan <shreyshriyan@gmail.com>
2023-05-30 22:21:25 +0530

    added working system tray applet

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,51 @@
-fn render(url: &str) -> wry::Result<()> {
-    use wry::{
-        application::{
-            event::{Event, StartCause, WindowEvent},
-            event_loop::{ControlFlow, EventLoop},
-            window::WindowBuilder,
-        },
-        webview::WebViewBuilder,
-    };
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        menu::{ContextMenu, MenuItemAttributes},
+        system_tray::{Icon, SystemTrayBuilder},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+};
 
-    let event_loop = EventLoop::new();
+fn load_icon(path: &std::path::Path) -> Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+}
+
+// fn add_tray(event_loop) {
+
+// }
+
+fn render(url: &str) -> wry::Result<()> {
+    // event loop
+    let event_loop: EventLoop<()> = EventLoop::new();
+
+    // add_tray(&event_loop);
+
+    let mut tray_menu = ContextMenu::new();
+
+    tray_menu.add_item(MenuItemAttributes::new("Quit"));
+
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/logo.png");
+
+    // let path = "src/assets/logo.png";
+
+    let icon = load_icon(std::path::Path::new(path));
+
+    let _system_tray = SystemTrayBuilder::new(icon.clone(), Some(tray_menu))
+        .build(&event_loop)
+        .unwrap();
+
+    // window logic
     let window = WindowBuilder::new()
         .with_title("Medium Reader")
         .build(&event_loop)?;
