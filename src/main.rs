856e3e67aec3df3c9f66b2c7f4a4799377a63943
shreyas-shriyan <shreyshriyan@gmail.com>
2023-06-01 23:58:13 +0530

    added tray event

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,75 @@ use wry::{
         menu::{ContextMenu, MenuItemAttributes},
         system_tray::{Icon, SystemTrayBuilder},
         window::WindowBuilder,
+        TrayId,
     },
     webview::WebViewBuilder,
 };
+
+fn render(url: &str) -> wry::Result<()> {
+    // event loop
+    let event_loop: EventLoop<()> = EventLoop::new();
+
+    let mut tray_menu = ContextMenu::new();
+
+    tray_menu.add_item(MenuItemAttributes::new("Quit"));
+
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/logo.png");
+
+    let icon = load_icon(std::path::Path::new(path));
+
+    let _system_tray = SystemTrayBuilder::new(icon.clone(), Some(tray_menu))
+        .with_id(TrayId::new("main-tray"))
+        .build(&event_loop)
+        .unwrap();
+
+    // window logic
+    let window = WindowBuilder::new()
+        .with_title("Medium Reader")
+        .build(&event_loop)?;
+
+    // webview logic
+    let _webview = WebViewBuilder::new(window)?.with_url(url)?.build()?;
+
+    // event loop
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::TrayEvent {
+                id,
+                // bounds,
+                // event,
+                // position,
+                ..
+            } => {
+                println!("{:?}", id)
+                // let tray = if id == "main" {
+                //     "main"
+                // } else if id == "second" {
+                //     "second"
+                // } else {
+                //     "unknown"
+                // };
+                // println!(
+                //     "tray `{}` event: {:?} {:?} {:?}",
+                //     tray, event, bounds, position
+                // );
+                // println!("event: {:?} {:?} {:?}", event, bounds, position);
+            }
+            _ => (),
+        }
+    });
+}
+
+fn main() {
+    let _page = render("https://medium.com/spatial-data-science/how-to-extract-locations-from-text-with-natural-language-processing-9b77035b3ea4");
+}
 
 fn load_icon(path: &std::path::Path) -> Icon {
     let (icon_rgba, icon_width, icon_height) = {
@@ -19,52 +85,4 @@ fn load_icon(path: &std::path::Path) -> Icon {
         (rgba, width, height)
     };
     Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
-}
-
-// fn add_tray(event_loop) {
-
-// }
-
-fn render(url: &str) -> wry::Result<()> {
-    // event loop
-    let event_loop: EventLoop<()> = EventLoop::new();
-
-    // add_tray(&event_loop);
-
-    let mut tray_menu = ContextMenu::new();
-
-    tray_menu.add_item(MenuItemAttributes::new("Quit"));
-
-    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/logo.png");
-
-    // let path = "src/assets/logo.png";
-
-    let icon = load_icon(std::path::Path::new(path));
-
-    let _system_tray = SystemTrayBuilder::new(icon.clone(), Some(tray_menu))
-        .build(&event_loop)
-        .unwrap();
-
-    // window logic
-    let window = WindowBuilder::new()
-        .with_title("Medium Reader")
-        .build(&event_loop)?;
-    let _webview = WebViewBuilder::new(window)?.with_url(url)?.build()?;
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
-}
-
-fn main() {
-    let _page = render("https://medium.com/spatial-data-science/how-to-extract-locations-from-text-with-natural-language-processing-9b77035b3ea4");
 }
