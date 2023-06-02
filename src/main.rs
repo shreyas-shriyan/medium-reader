fa75d62df87ab6b1af550ed9ff729608ebd693b9
shreyas-shriyan <shreyshriyan@gmail.com>
2023-06-03 02:25:23 +0530

    working clipboard reader, removed system tray code

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,33 @@ use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
-        menu::{ContextMenu, MenuItemAttributes},
-        system_tray::{Icon, SystemTrayBuilder},
+        // menu::{ContextMenu, MenuItemAttributes},
+        // system_tray::{Icon, SystemTrayBuilder},
         window::WindowBuilder,
-        TrayId,
+        // TrayId,
     },
     webview::WebViewBuilder,
 };
 
-use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use cli_clipboard;
+
+fn main() {
+    let url: String = read_clipboard();
+    let _page = render(&url);
+}
+
+fn read_clipboard() -> String {
+    let mut clipboard_data = cli_clipboard::get_contents().unwrap();
+    if clipboard_data.is_empty() {
+        // if clip
+        clipboard_data = "https://medium.com/".to_string()
+    }
+    return clipboard_data;
+}
 
 fn render(url: &str) -> wry::Result<()> {
-    let mut ctx = ClipboardContext::new().unwrap();
-
-    let clipboardContent = ctx.get_contents();
-
-    println!("{:?}", clipboardContent);
-
     // event loop
     let event_loop: EventLoop<()> = EventLoop::new();
-
-    let mut tray_menu = ContextMenu::new();
-
-    tray_menu.add_item(MenuItemAttributes::new("Quit"));
-
-    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/logo.png");
-
-    let icon = load_icon(std::path::Path::new(path));
-
-    let _system_tray = SystemTrayBuilder::new(icon.clone(), Some(tray_menu))
-        .with_id(TrayId::new("main-tray"))
-        .build(&event_loop)
-        .unwrap();
 
     // window logic
     let window = WindowBuilder::new()
@@ -53,44 +48,34 @@ fn render(url: &str) -> wry::Result<()> {
                 event: WindowEvent::CloseRequested,
                 ..
             } => *control_flow = ControlFlow::Exit,
-            Event::TrayEvent {
-                id,
-                // bounds,
-                // event,
-                // position,
-                ..
-            } => {
-                println!("{:?}", id)
-                // let tray = if id == "main" {
-                //     "main"
-                // } else if id == "second" {
-                //     "second"
-                // } else {
-                //     "unknown"
-                // };
-                // println!(
-                //     "tray `{}` event: {:?} {:?} {:?}",
-                //     tray, event, bounds, position
-                // );
-                // println!("event: {:?} {:?} {:?}", event, bounds, position);
-            }
             _ => (),
         }
     });
 }
 
-fn main() {
-    let _page = render("https://medium.com/spatial-data-science/how-to-extract-locations-from-text-with-natural-language-processing-9b77035b3ea4");
-}
+// fn tray_init(&event_loop) {
+//     let mut tray_menu = ContextMenu::new();
 
-fn load_icon(path: &std::path::Path) -> Icon {
-    let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open(path)
-            .expect("Failed to open icon path")
-            .into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        (rgba, width, height)
-    };
-    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
-}
+//     tray_menu.add_item(MenuItemAttributes::new("Quit"));
+
+//     let path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/assets/logo.png");
+
+//     let icon = load_icon(std::path::Path::new(path));
+
+//     let _system_tray = SystemTrayBuilder::new(icon.clone(), Some(tray_menu))
+//         .with_id(TrayId::new("main-tray"))
+//         .build(&event_loop)
+//         .unwrap();
+// }
+
+// fn load_icon(path: &std::path::Path) -> Icon {
+//     let (icon_rgba, icon_width, icon_height) = {
+//         let image = image::open(path)
+//             .expect("Failed to open icon path")
+//             .into_rgba8();
+//         let (width, height) = image.dimensions();
+//         let rgba = image.into_raw();
+//         (rgba, width, height)
+//     };
+//     Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+// }
