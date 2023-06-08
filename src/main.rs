ddf1c67b24a587e06388fed2959249cd80aeb78d
shreyas-shriyan <shreyshriyan@gmail.com>
2023-06-08 23:37:11 +0530

    handled custom medium domains

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use wry::{
     webview::WebViewBuilder,
 };
 
-use url::{Host, Url};
+use url::Url;
 
 fn main() {
     let url: String = read_clipboard();
@@ -20,24 +20,14 @@ fn main() {
 
 fn read_clipboard() -> String {
     gtk::init().unwrap();
+
     let cliboard = Clipboard::new();
 
-    let mut content = cliboard.read_text().unwrap();
+    let content = cliboard.read_text().unwrap();
 
-    let default_url: String = "https://medium.com".to_string();
+    let webview_url = generate_url(content);
 
-    if content.is_empty() {
-        content = default_url
-    } else {
-        let url_parsed_data = Url::parse(&content).unwrap();
-
-        if url_parsed_data.host() == Some(Host::Domain("medium.com")) {
-            content = url_parsed_data.to_string();
-        } else {
-            content = default_url
-        }
-    }
-    return content.to_string();
+    return webview_url;
 }
 
 fn render(url: &str) -> wry::Result<()> {
@@ -65,6 +55,42 @@ fn render(url: &str) -> wry::Result<()> {
             _ => (),
         }
     });
+}
+
+fn generate_url(content: String) -> String {
+    let url = Url::parse("https://medium.com").unwrap();
+
+    let url_parse_data = Url::parse(&content).unwrap_or(url);
+
+    let mut parsed_url = url_parse_data.host_str().unwrap();
+
+    let medium_custom_domains: Vec<&str> = vec![
+        "medium.com",
+        "blog.discord.com",
+        "uxdesign.cc",
+        "towardsdatascience.com",
+        "hackernoon.com",
+        "medium.freecodecamp.org",
+        "betterhumans.coach.me",
+        "codeburst.io",
+        "medium.mybridge.co",
+        "uxdesign.cc",
+        "levelup.gitconnected.com",
+        "itnext.io",
+        "entrepreneurshandbook.co",
+        "proandroiddev.com",
+        "blog.prototypr.io",
+        "thebolditalic.com",
+        "blog.usejournal.com",
+    ];
+
+    if !medium_custom_domains.contains(&parsed_url) {
+        parsed_url = "https://medium.com"
+    } else {
+        parsed_url = url_parse_data.as_str()
+    }
+
+    return parsed_url.to_string();
 }
 
 // fn tray_init(&event_loop) {
